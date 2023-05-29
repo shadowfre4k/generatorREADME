cc65380a5f0f1a3e81d02f16cefd226a70e62ac9
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-05-29 10:18:35 -0700

    added array of questions, installed inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter the title of your project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a desciption.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter installation instructions.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter how to run your project.",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please enter contributions for your project.",
+  },
+  {
+    type: "input",
+    name: "testing",
+    message: "Please enter test instructions for your project.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select a license for your project.",
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT",
+      "BSD 2-Clause 'Simplified' License",
+      "BSD 3-Clause 'New' or 'Revived' License",
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+    ],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Please enter your email address.",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
