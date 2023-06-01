6e3714e28db7d7073bb3e20254cf1b6cc7c0a30d
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-05-31 17:51:32 -0700

    added renderLicense selection and  a bit of markdown before testing

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -45,7 +46,8 @@ const questions = [
       "BSD 3-Clause 'New' or 'Revived' License",
       "Boost Software License 1.0",
       "Creative Commons Zero v1.0 Universal",
-      "Eclipse Public License 2.0",
+      "Eclipse Public License 1.0",
+      "None",
     ],
   },
   {
@@ -62,7 +64,9 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile();
+  fs.writeFile(fileName, generateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log(`Success`)
+  );
 }
 
 // TODO: Create a function to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,22 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
+    case "Apache License 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU General Public License v3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
     case "MIT":
       return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    case "ISC":
-      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-    case "Apache":
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    case "GPL":
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-    case "BSD":
-      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    case "BSD 2-Clause 'Simplified' License":
+      return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+    case "BSD 3-Clause 'New' or 'Revived' License":
+      return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    case "Boost Software License 1.0":
+      return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    case "Creative Commons Zero v1.0 Universal":
+      return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    case "Eclipse Public License 1.0":
+      return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
     case "None":
       return "";
     default:
@@ -25,12 +31,81 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  {
+    // switch case for different licenses but since this is a reference they all say the same thing however
+    // expected to have different returns for actual license
+    switch (license) {
+      case "Apache License 2.0":
+        return `This Project is licensed under the ${license} license.`;
+      case "GNU General Public License v3.0":
+        return `This Project is licensed under the ${license} license.`;
+      case "MIT":
+        return `This Project is licensed under the ${license} license.`;
+      case "BSD 2-Clause 'Simplified' License":
+        return `This Project is licensed under the ${license} license.`;
+      case "BSD 3-Clause 'New' or 'Revived' License":
+        return `This Project is licensed under the ${license} license.`;
+      case "Boost Software License 1.0":
+        return `This Project is licensed under the ${license} license.`;
+      case "Creative Commons Zero v1.0 Universal":
+        return `This Project is licensed under the ${license} license.`;
+      case "Eclipse Public License 1.0":
+        return `This Project is licensed under the ${license} license.`;
+      case "None":
+        return "";
+      default:
+        return "";
+    }
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+  #Demo
+
+##Contributions
+
+${data.contribution}
+
+## License
+
+${data.license}
+
+## Badges
+
+!${data.badges}
+
+
+
+## Questions
+
+If you have any questions please contact me with the information below!
+
+${data.emailAddress}
+https://github.com/shadowfre4k${data.username}
 `;
 }
 
